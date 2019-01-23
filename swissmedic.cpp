e2cbd12af386193d88895864daa8ace7a8456b9b
Alessandro Bettarini <abettarini@icloud.com>
2019-01-23 16:35:35 +0100

    swissmedic: skip header, pre-pad with 0s column A and K

--- swissmedic.cpp
+++ swissmedic.cpp
@@ -18,10 +18,14 @@
 #define COLUMN_K       10   // packaging code (3 digits)
 #define COLUMN_N       13   // category (A..E)
 
+#define FIRST_DATA_ROW_INDEX    5
+
 namespace SWISSMEDIC
 {
     
-std::vector< std::vector<std::string> > theWholeSpreadSheet;
+    std::vector< std::vector<std::string> > theWholeSpreadSheet;
+    std::vector<std::string> regnrs;        // padded to 5 characters (digits)
+    std::vector<std::string> packingCode;   // padded to 3 characters (digits)
 
 void parseXLXS(const std::string &filename)
 {
@@ -31,7 +35,11 @@ void parseXLXS(const std::string &filename)
 
     std::clog << std::endl << "Reading swissmedic XLSX" << std::endl;
 
+    int skipHeaderCount = 0;
     for (auto row : ws.rows(false)) {
+        if (++skipHeaderCount <= FIRST_DATA_ROW_INDEX)
+            continue;
+
         std::vector<std::string> aSingleRow;
         for (auto cell : row) {
             //std::clog << cell.to_string() << std::endl;
@@ -39,6 +47,20 @@ void parseXLXS(const std::string &filename)
         }
 
         theWholeSpreadSheet.push_back(aSingleRow);
+
+        // Precalculate padded regnr
+        std::string rn5 = aSingleRow[COLUMN_A];
+        while (rn5.length() < 5) // pad with zeros
+            rn5 = "0" + rn5;
+
+        regnrs.push_back(rn5);
+
+        // Precalculate padded packing code
+        std::string code3 = aSingleRow[COLUMN_K];
+        while (code3.length() < 3) // pad with zeros
+            code3 = "0" + code3;
+        
+        packingCode.push_back(code3);
     }
 
     std::clog << "swissmedic rows: " << theWholeSpreadSheet.size() << std::endl;
@@ -51,7 +73,8 @@ std::string getNames(const std::string &rn)
     std::string names;
     int i=0;
     for (int rowInt = 0; rowInt < theWholeSpreadSheet.size(); rowInt++) {
-        std::string gtin_5 = theWholeSpreadSheet.at(rowInt).at(COLUMN_A);
+        std::string gtin_5 = regnrs[rowInt];
+
         // TODO: to speed up do a numerical comparison so that we can return when gtin5>rn
         // assuming that column A is sorted
         if (gtin_5 == rn) {
@@ -60,7 +83,7 @@ std::string getNames(const std::string &rn)
 
             //names += "swm-";
             names += theWholeSpreadSheet.at(rowInt).at(COLUMN_C);
-            //std::cerr << " FOUND at: " << rowInt << ", name " << name << std::endl;
+            //std::clog << " FOUND at: " << rowInt << ", name " << name << std::endl;
             i++;
         }
     }
@@ -77,10 +100,10 @@ int countRowsWithRn(const std::string &rn)
 {
     int count = 0;
     for (int rowInt = 0; rowInt < theWholeSpreadSheet.size(); rowInt++) {
-        std::string gtin5 = theWholeSpreadSheet.at(rowInt).at(COLUMN_A);
+        std::string gtin_5 = regnrs[rowInt];
         // TODO: to speed up do a numerical comparison so that we can return when gtin5>rn
         // assuming that column A is sorted
-        if (gtin5 == rn)
+        if (gtin_5 == rn)
             count++;
     }
 
@@ -106,17 +129,17 @@ int getChecksum(std::string eanStr)
     
     return checksum_digit;
 }
-    
+
 bool findGtin(const std::string &gtin)
 {
     for (int rowInt = 0; rowInt < theWholeSpreadSheet.size(); rowInt++) {
-        std::string regnr = theWholeSpreadSheet.at(rowInt).at(COLUMN_A);
-        std::string code = theWholeSpreadSheet.at(rowInt).at(COLUMN_K);
-        std::string gtin_13 = "7680" + regnr + code;
+        std::string rn5 = regnrs[rowInt];
+        std::string code3 = packingCode[rowInt];
+        std::string gtin13 = "7680" + rn5 + code3;
 #if 0
         // TODO: use checksum
 #else
-        if (gtin == gtin.substr(0,12)) // pos, len
+        if (gtin13 == gtin.substr(0,12)) // pos, len
             return true;
 #endif
     }
