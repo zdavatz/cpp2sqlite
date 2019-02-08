73430ec60415743c2e5a10bc91eeadc16b7d57af
Alessandro Bettarini <abettarini@icloud.com>
2019-02-08 14:40:58 +0100

    fix error in submodule ean13

--- cpp2sqlite.cpp
+++ cpp2sqlite.cpp
@@ -26,6 +26,7 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/foreach.hpp>
+#include <boost/program_options/errors.hpp>
 
 #include "aips.hpp"
 #include "refdata.hpp"
