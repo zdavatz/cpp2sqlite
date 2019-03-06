307f41567709153024d61fbe9ebbf5560f77da4f
Alessandro Bettarini <abettarini@icloud.com>
2019-03-06 11:57:44 +0100

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,19 +12,21 @@ SRC_DIR=$(pwd)/..
 BLD_DIR="$SRC_DIR/build"
 BIN_DIR=/usr/local/bin/
 
+BIN_XLNT=/usr/local
+
 rm -rf $BLD_DIR
 mkdir -p $BLD_DIR
 cd $BLD_DIR
 
-cmake -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$BIN_DIR \
-    -DXLNT_DIR=/usr/local \
-    -D XLNT_LIBRARY_DIRS=/usr/local \
+cmake -G"Unix Makefiles" \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=$BIN_DIR \
+    -D XLNT_DIR=$BIN_XLNT \
     $SRC_DIR
 
 make -j9
 #sudo make install
 
 cd $BLD_DIR  # it should be $BIN_DIR otherwise there is no point in doing make install
-time cpp2sqlite --verbose --lang=fr --inDir $SRC_DIR/input
-#time cpp2sqlite --verbose --inDir $SRC_DIR/input
+time ./cpp2sqlite --verbose --lang=fr --inDir $SRC_DIR/input
+#time ./cpp2sqlite --verbose --inDir $SRC_DIR/input
