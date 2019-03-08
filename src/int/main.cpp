1612ee5998feb51d7ad71e791c6f4353a8cc3a63
Alessandro Bettarini <abettarini@icloud.com>
2019-03-08 17:57:50 +0100

    Issue #46

--- src/int/main.cpp
+++ src/int/main.cpp
@@ -8,14 +8,114 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <libgen.h>     // for basename()
+
+#include <boost/algorithm/string_regex.hpp>
+
+#define OUTPUT_FILE_SEPARATOR   "|"
+
+#define inColumnA     cols[0]
+#define inColumnB     cols[1]
+#define inColumnC     cols[2]
+#define inColumnD     cols[3]
+#define inColumnE     cols[4]
+#define inColumnF     cols[5]
+#define inColumnG     cols[6]
+#define inColumnH     cols[7]
+#define inColumnI     cols[8]
+
+void outputInteraction(std::ofstream &ofs,
+                       const std::vector<std::string> &cols)
+{
+    if (cols.size() != 9) {
+        std::clog << "Unexpected # columns: " << cols.size() << std::endl;
+        return;
+    }
+
+    std::string nameSection1 = "Risikoklasse";
+    std::string nameSection2 = "Möglicher Effekt";
+    std::string nameSection3 = "Mechanismus";
+    std::string nameSection4 = "Empfohlene Massnahmen";
+    
+    std::string legend; // TODO: it depends on column I
+    if (inColumnI == "D")
+        legend = "Kombination vermeiden";
+
+    std::string outColumnE = "<div><div class=\"paragraph" + inColumnI + "\" id=\"" + inColumnA + "-" + inColumnC + "\"><div class=\"absTitle\">" + inColumnA + " [" + inColumnB + "] &rarr;";
+    
+    outColumnE += " " + inColumnC + " [" + inColumnD + "]</div></div><p class=\"spacing2\"><i>" + nameSection1 + ":</i> " + legend + " (" + inColumnI + ")</p><p class=\"spacing2\"><i>" + nameSection2 + ":</i> " + inColumnE + "</p><p class=\"spacing2\"><i>" + nameSection3 + ":</i> " + inColumnF + "</p><p class=\"spacing2\"><i>" + nameSection4 + ":</i> " + inColumnH + "</p></div>";
+
+#if 0
+    std::clog
+#else
+    ofs
+#endif
+    << inColumnA << OUTPUT_FILE_SEPARATOR   // A
+    << OUTPUT_FILE_SEPARATOR                // B empty column
+    << inColumnC << OUTPUT_FILE_SEPARATOR   // C
+    << OUTPUT_FILE_SEPARATOR                // D empty column
+    << outColumnE
+    << std::endl;
+}
 
 void parseCSV(const std::string &filename,
               const std::string &language,
               bool verbose)
 {
+    std::ofstream ofs;
+    
+    std::string logDir("./");
+    std::string fullFilename(logDir);
+    fullFilename += "drug_interaction_" + language + ".csv";
+    ofs.open(fullFilename.c_str());
+
+    try {
+        std::clog << std::endl << "Reading CSV" << std::endl;
+        std::ifstream file(filename);
+        
+        std::string str;
+        bool header = true;
+        while (std::getline(file, str)) {
+            
+            std::vector<std::string> columnVector;
+            std::vector<std::string> columnTrimmedVector;
+            //boost::algorithm::split(interVector, str, boost::is_any_of("\","), boost::token_compress_on);
+            boost::algorithm::split_regex( columnVector, str, boost::regex( "\"," ));
+
+            for (auto s : columnVector) {
+                boost::algorithm::trim_left_if(s, boost::is_any_of("\""));
+                boost::algorithm::trim_right_if(s, boost::is_any_of("\""));
+                columnTrimmedVector.push_back(s);
+            }
+
+            if (header) {
+                header = false;
+                continue;
+            }
+            
+            outputInteraction(ofs, columnTrimmedVector);
+        }
+    }
+    catch (std::exception &e) {
+        std::cerr
+        << basename((char *)__FILE__) << ":" << __LINE__
+        << " Error " << e.what()
+        << std::endl;
+    }
+    
+    ofs.close();
 }
 
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        std::cout << "Missing CSV filename" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    parseCSV(argv[1], "de", false);
     return EXIT_SUCCESS;
 }
