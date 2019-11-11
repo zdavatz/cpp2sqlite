317f5e7051d009700974e7228b9e231a9db61515
Alessandro Bettarini <abettarini@icloud.com>
2019-11-11 11:38:39 +0100

    Issue #89 one more input file converted to UTF8 before using it

--- scripts/download_private.sh
+++ scripts/download_private.sh
@@ -54,7 +54,11 @@ curl -o artikel_stamm_zurrose.csv -O "${URL}"/Artikelstamm.csv --user "${USERNAM
 curl -o artikel_stamm_voigt.csv -O "${URL}"/Artikelstamm_Voigt.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
 
 # Updated every 30 minutes
-curl -o artikel_vollstamm_zurrose.csv -O "${URL}"/Artikelstamm_Vollstamm.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+
+ISO_8859_1_FILE=artikel_temp.csv
+curl -o $ISO_8859_1_FILE -O "${URL}"/Artikelstamm_Vollstamm.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+iconv -f ISO-8859-1 -t UTF-8 $ISO_8859_1_FILE >artikel_vollstamm_zurrose.csv
+rm $ISO_8859_1_FILE
 
 curl -o direct_subst_zurrose.csv -O "${URL}"/direktsubstitution.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
 curl -o nota_zurrose.csv -O "${URL}"/Nota.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
