b4d9821f193a047e2248203ac151f7af6d7d4709
Alessandro Bettarini <abettarini@icloud.com>
2019-12-16 11:26:51 +0100

    Issue #111

--- scripts/download_zr.sh
+++ scripts/download_zr.sh
@@ -55,7 +55,10 @@ curl -o $ISO_8859_1_FILE -O "${URL}"/Kunden_alle.csv --user "${USERNAME_ZUR}:${P
 iconv -f ISO-8859-1 -t UTF-8 $ISO_8859_1_FILE >Kunden_alle.csv
 rm $ISO_8859_1_FILE
 
-curl -O "${URL}"/Kunden_alle_NEU.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+ISO_8859_1_FILE=Kunden_NEU_temp.csv
+curl -o $ISO_8859_1_FILE -O "${URL}"/Kunden_alle_NEU.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+iconv -f ISO-8859-1 -t UTF-8 $ISO_8859_1_FILE >Kunden_alle_NEU.csv
+rm $ISO_8859_1_FILE
 
 popd
 
