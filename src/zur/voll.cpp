85645d398d4978d353cef8c7a2bde8d11d6cee68
b123400 <i@b123400.net>
2024-02-04 11:24:55 +0900

    Fix unit regex to include space
    https://github.com/zdavatz/smart-order/issues/134

--- src/zur/voll.cpp
+++ src/zur/voll.cpp
@@ -94,7 +94,7 @@ std::string parseUnitFromTitle(const std::string &pack_title)
     }
 
     std::string dosage2;
-    std::regex rgx2(R"(((\d+)(\.\d+)?(Ds|ds|mg)?)(\/(\d+)(\.\d+)?(Ds|ds|mg|ml|mg|g)?)+)");  // tested at https://regex101.com
+    std::regex rgx2(R"(((\d+)(\.\d+)?(Ds|ds|mg)?)(\/(\d+)(\.\d+)?\s*(Ds|ds|mg|ml|mg|g)?)+)");  // tested at https://regex101.com
     if (std::regex_search(pack_title, match, rgx2)) {
         dosage2 = match[0];
     }
