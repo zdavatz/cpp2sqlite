dcd437fd1ddb87000575b22b1acfd79a77398cda
b123400 <i@b123400.net>
2025-07-07 15:25:00 +0900

    Use sshpass and scp for sftp instead of ftp

--- scripts/download_zr.sh
+++ scripts/download_zr.sh
@@ -4,10 +4,11 @@
 WD=$(pwd)
 SRC_DIR=$(realpath ../)
 
+DOMAIN="ftp.zur-rose.ch"
 if [ "test" == "$1" ] ; then
-    URL="ftp://ftp.zur-rose.ch/ywesee%20OutTest"
+    DIR="/ywesee OutTest"
 else
-    URL="ftp://ftp.zur-rose.ch/ywesee%20Out"
+    DIR="/ywesee Out"
 fi
 
 
@@ -26,35 +27,34 @@ mkdir -p $ZURROSE_DIR
 pushd $ZURROSE_DIR
 
 # Updated once per day
-curl -o artikel_stamm_zurrose.csv -O "${URL}"/Artikelstamm.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/Artikelstamm.csv" artikel_stamm_zurrose.csv
 
 # Updated every 4 hours
-curl -o artikel_stamm_voigt.csv -O "${URL}"/Artikelstamm_Voigt.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/Artikelstamm_Voigt.csv" artikel_stamm_voigt.csv
 
 # Updated every 30 minutes
 ISO_8859_1_FILE=artikel_temp.csv
-curl -o $ISO_8859_1_FILE -O "${URL}"/Artikelstamm_Vollstamm.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/Artikelstamm_Vollstamm.csv" "$ISO_8859_1_FILE"
 iconv -f ISO-8859-1 -t UTF-8 $ISO_8859_1_FILE >artikel_vollstamm_zurrose.csv
 rm $ISO_8859_1_FILE
 
-curl -o direct_subst_zurrose.csv -O "${URL}"/direktsubstitution.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
-curl -o nota_zurrose.csv -O "${URL}"/Nota.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
-curl -o galenic_codes_map_zurrose.txt -O "${URL}"/Vollstamm_Galenic_Form_Mapping_by_Code.txt --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/direktsubstitution.csv" "direct_subst_zurrose.csv"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/Nota.csv" "nota_zurrose.csv"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/Vollstamm_Galenic_Form_Mapping_by_Code.txt" "galenic_codes_map_zurrose.txt"
 
 #curl -O "${URL}"/_log.txt --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
-curl -O "${URL}"/Autogenerika.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/Autogenerika.csv" "Autogenerika.csv"
 
 ISO_8859_1_FILE=Kunden_NEU_temp.csv
-curl -o $ISO_8859_1_FILE -O "${URL}"/kunden_alle_dynamics_ce.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/kunden_alle_dynamics_ce.csv" "$ISO_8859_1_FILE"
+
 iconv -f ISO-8859-1 -t UTF-8 $ISO_8859_1_FILE >kunden_alle_dynamics_ce.csv
 rm $ISO_8859_1_FILE
 
-curl -o medix_kunden.csv -O "${URL}"/medix_kunden.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/medix_kunden.csv" "medix_kunden.csv"
 
-curl -o Grippeimpfstoff.csv -O "${URL}"/Grippeimpfstoff.csv --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/Grippeimpfstoff.csv" "Grippeimpfstoff.csv"
 
-curl -o 19er_pharmaCodes.csv -O "${URL}/19er_pharmaCodes.csv" --user "${USERNAME_ZUR}:${PASSWORD_ZUR}"
+sshpass -p "$PASSWORD_ZUR" scp -v "$USERNAME_ZUR@$DOMAIN:$DIR/19er_pharmaCodes.csv" "19er_pharmaCodes.csv"
 
 popd
-
-
